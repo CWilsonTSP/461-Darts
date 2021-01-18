583f88619a8e966ea7eaff2e02ce825ba9944808
TheSaltyPug <cmdsmithpc@gmail.com>
2021-01-18 11:50:58 -0500

    added comments, double function, and lists of numbers

--- darts.py
+++ darts.py
@@ -1,8 +1,19 @@
 # Chad Wilson
 # Add your names here
 
+# functions
+def double(lst):
+    return [x * 2 for x in lst]
+
+# global variables
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,25]
+doubled_numbers = double(numbers)
+
+
+# main
 def main():
-    print("Hello, world!")
+    print(numbers)
+    print(doubled_numbers)
     return
 
 
