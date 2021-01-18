de6d0ce8b026f50556436234afc65c274b6ee30d
Justin Moore <justin.moore1@live.longwood.edu>
2021-01-18 11:58:24 -0500

    Added triples list

--- darts.py
+++ darts.py
@@ -5,15 +5,19 @@
 def double(lst):
     return [x * 2 for x in lst]
 
+def triple(lst):
+	return [x * 3 for x in lst[:-1]]
+
 # global variables
 numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,25]
 doubled_numbers = double(numbers)
-
+tripled_numbers = triple(numbers)
 
 # main
 def main():
     print(numbers)
     print(doubled_numbers)
+    print(tripled_numbers)
     return
 
 
